3b0221c4cc425fb5e63b8e43b092c7c6a652effb
tfriese <thomas.friese@tuhh.de>
2024-08-15 01:25:28 +0200

    minor testing, but it seems like it SOMEHOW FUCKING WORKS OOOO:::

--- main.cpp
+++ main.cpp
@@ -1,38 +1,30 @@
 #include <iostream>
 #include <Dense>
 
-#include "ray.hpp"
+#include <ray.hpp>
+#include <objects.hpp>
+#include "math.h"
 #include "defines.h"
 
 int main() {
-  //Eigen example
-  Eigen::DiagonalMatrix<float, 3> A(4.5, 3.2, 1.1);
   
-  std::cout << A << "\n\n" << ((Eigen::Transform<float, 3, Eigen::Projective>) A).matrix() << std::endl;
-  
-  // Eigen::AngleAxis<float> B(2.1, Eigen::Vector3f::UnitX());
+  HalfSpace* H = new HalfSpace();
+  Transformation* rot = Transformation::Rotation_X(H, M_PI_2);
 
-  // Eigen::Transform<float, 3, Eigen::Projective> C = (Eigen::DiagonalMatrix<float, 3>) A.inverse().diagonal() * B.inverse();
+  RootObject* R = new RootObject(rot);
 
-  // Eigen::Translation<float, 3> D(2.1, 1.2, 3.0);
-  // C *= D.inverse();
+  Eigen::Vector3f S(1, 1, 0);
+  Eigen::Vector3f d(sqrtf(2)/2, -sqrtf(2)/2, 0);
 
-  // std::cout << C.matrix() << std::endl;
+  Ray r(S, d, 1.0);
 
-  //Ray example
-  // Eigen::Vector3f S(-1, 1, 1), P(0, 0, 0);
-  // Eigen::Vector3f d(1, -1, 0), normal(0, 1, 0);
-  // Ray r(S, d, 1.0);
-
-  // std::cout << r.start_point() << "\n" << r.direction() << "\n\n\n";
-
-  // Ray t = r.reflect(P, normal);
-
-  // std::cout << t.start_point() << "\n" << t.direction() << std::endl;
-
-  // Ray f = r.refract(P, normal, 2.3);
-
-  // std::cout << f.start_point() << "\n" << f.direction() << std::endl; 
+  IntersectionPoint P;
+  if (R->intersect(r, P)) {
+    std::cout << P.point << std::endl;
+  }
+  else {
+    std::cout << "NO INTERSECTION" << std::endl;
+  }
 
   return 0;
 }
