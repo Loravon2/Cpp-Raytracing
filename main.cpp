6d24087cbac05d21d59f3745f6f219b61d9bd8a1
tfriese <thomas.friese@tuhh.de>
2024-08-15 00:55:33 +0200

    minor adjustments

--- main.cpp
+++ main.cpp
@@ -7,29 +7,32 @@
 int main() {
   //Eigen example
   Eigen::DiagonalMatrix<float, 3> A(4.5, 3.2, 1.1);
-  Eigen::AngleAxis<float> B(2.1, Eigen::Vector3f::UnitX());
+  
+  std::cout << A << "\n\n" << ((Eigen::Transform<float, 3, Eigen::Projective>) A).matrix() << std::endl;
+  
+  // Eigen::AngleAxis<float> B(2.1, Eigen::Vector3f::UnitX());
 
-  Eigen::Transform<float, 3, Eigen::Projective> C = (Eigen::DiagonalMatrix<float, 3>) A.inverse().diagonal() * B.inverse();
+  // Eigen::Transform<float, 3, Eigen::Projective> C = (Eigen::DiagonalMatrix<float, 3>) A.inverse().diagonal() * B.inverse();
 
-  Eigen::Translation<float, 3> D(2.1, 1.2, 3.0);
-  C *= D.inverse();
+  // Eigen::Translation<float, 3> D(2.1, 1.2, 3.0);
+  // C *= D.inverse();
 
-  //std::cout << C.matrix() << std::endl;
+  // std::cout << C.matrix() << std::endl;
 
   //Ray example
-  Eigen::Vector3f S(-1, 1, 1), P(0, 0, 0);
-  Eigen::Vector3f d(1, -1, 0), normal(0, 1, 0);
-  Ray r(S, d);
+  // Eigen::Vector3f S(-1, 1, 1), P(0, 0, 0);
+  // Eigen::Vector3f d(1, -1, 0), normal(0, 1, 0);
+  // Ray r(S, d, 1.0);
 
-  std::cout << r.get_S() << "\n" << r.get_d() << "\n\n\n";
+  // std::cout << r.start_point() << "\n" << r.direction() << "\n\n\n";
 
-  Ray t = r.reflect(P, normal);
+  // Ray t = r.reflect(P, normal);
 
-  std::cout << t.get_S() << "\n" << t.get_d() << std::endl;
+  // std::cout << t.start_point() << "\n" << t.direction() << std::endl;
 
-  Ray f = r.refract(P, normal, 1.0, 2.3);
+  // Ray f = r.refract(P, normal, 2.3);
 
-  std::cout << f.get_S() << "\n" << f.get_d() << std::endl; 
+  // std::cout << f.start_point() << "\n" << f.direction() << std::endl; 
 
   return 0;
 }
