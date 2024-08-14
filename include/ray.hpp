d354cb6d1c7218006e8b092df43b6d4e5c560643
tfriese <thomas.friese@tuhh.de>
2024-08-15 00:54:43 +0200

    a few adjustments / new operator

--- include/ray.hpp
+++ include/ray.hpp
@@ -7,18 +7,22 @@ class Ray {
 private:
   Eigen::Vector4f S;
   Eigen::Vector4f d;
+  float n;
 public:
-  Ray(Eigen::Vector3f S, Eigen::Vector3f d);
-  Ray(Eigen::Vector4f S, Eigen::Vector4f d);
-  Ray(float sx, float sy, float sz, float dx, float dy, float dz);
+  Ray(Eigen::Vector3f S, Eigen::Vector3f d, float n);
+  Ray(Eigen::Vector4f S, Eigen::Vector4f d, float n);
+  Ray(float sx, float sy, float sz, float dx, float dy, float dz, float n);
   Ray();
 
-  const Eigen::Vector4f& get_S() const;
-  const Eigen::Vector4f& get_d() const;
+  const Eigen::Vector4f& start_point() const;
+  const Eigen::Vector4f& direction() const;
+  float index() const;
 
   const Ray reflect(const Eigen::Vector4f& P, Eigen::Vector4f normal) const;
   const Ray reflect(const Eigen::Vector3f& P, const Eigen::Vector3f& normal) const;
 
-  const Ray refract(const Eigen::Vector4f& P, Eigen::Vector4f normal, float n1, float n2);
-  const Ray refract(const Eigen::Vector3f& P, const Eigen::Vector3f& normal, float n1, float n2);
+  const Ray refract(const Eigen::Vector4f& P, Eigen::Vector4f normal, float n2);
+  const Ray refract(const Eigen::Vector3f& P, const Eigen::Vector3f& normal, float n2);
 };
+
+const Ray operator*(const Eigen::Transform<float, 3, Eigen::Projective>& T, const Ray& r);
