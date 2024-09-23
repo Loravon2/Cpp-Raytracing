124cc4a1c2b7e8819ee4107fc221f583f614c5b1
cur9311 <karamoko.assamoi@tuhh.de>
2024-09-23 05:42:06 +0200

    added a triangle (isosceles is the one you should use) and a cube with a pattern

--- include/composite.hpp
+++ include/composite.hpp
@@ -4,5 +4,7 @@
 class Composites: public BaseObject{
 public:
   static Intersection* cube(ColData col, float index);
-  static BaseObject* triangle(ColData col, float index);
+  static Union* cube_pattern(ColData col, float index);
+  static Subtraction* triangle_isosceles(ColData col, float index);
+  static Subtraction* triangle_equilateral(ColData col, float index);
 };
